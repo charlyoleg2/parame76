8916a492b47dece1c36f9043842d78c501adfbc8
charlyoleg <charles.braquet@gmx.fr>
2025-08-21 11:05:31 +0200

    ref circle

--- pkg/desi76/src/stone/stairs.ts
+++ pkg/desi76/src/stone/stairs.ts
@@ -20,10 +20,10 @@ import {
 	//point,
 	//contour,
 	contourCircle,
-	ctrRectangle,
+	//ctrRectangle,
 	figure,
 	//degToRad,
-	//radToDeg,
+	radToDeg,
 	ffix,
 	pNumber,
 	//pCheckbox,
@@ -43,8 +43,10 @@ const pDef: tParamDef = {
 		pNumber('Nn', 'stair', 20, 1, 200, 1),
 		pNumber('Nd', 'stair', 40, 1, 200, 1),
 		pNumber('D1', 'mm', 5000, 1000, 50000, 1),
-		pNumber('W1', 'mm', 2000, 400, 10000, 1),
-		pNumber('W2', 'mm', 4000, 400, 10000, 1),
+		pNumber('Wi1', 'mm', 1000, 1, 10000, 1),
+		pNumber('We1', 'mm', 1000, 1, 10000, 1),
+		pNumber('Wi2', 'mm', 2000, 1, 10000, 1),
+		pNumber('We2', 'mm', 2000, 1, 10000, 1),
 		pSectionSeparator('Details'),
 		pDropdown('border', ['arc', 'straight']),
 		pNumber('H1', 'mm', 20, 10, 500, 1),
@@ -55,8 +57,10 @@ const pDef: tParamDef = {
 		Nn: 'stairs_top.svg',
 		Nd: 'stairs_top.svg',
 		D1: 'stairs_top.svg',
-		W1: 'stairs_top.svg',
-		W2: 'stairs_top.svg',
+		Wi1: 'stairs_top.svg',
+		We1: 'stairs_top.svg',
+		Wi2: 'stairs_top.svg',
+		We2: 'stairs_top.svg',
 		border: 'stairs_top.svg',
 		H1: 'stairs_height.svg',
 		Wc: 'stairs_top.svg',
@@ -72,40 +76,57 @@ const pDef: tParamDef = {
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
 	const figTop = figure();
-	const figBorderL = figure();
-	const figBorderS = figure();
+	const figBorderI = figure();
+	const figBorderE = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
+		const pi = Math.PI;
+		//const pi2 = pi / 2;
 		const R1 = param.D1 / 2;
+		const Wic = (param.Wi2 - param.Wi1) / param.Nn;
+		const Wec = (param.We2 - param.We1) / param.Nn;
+		const aStair2 = pi / param.Nd;
+		const Rid = Wic / Math.sin(aStair2);
+		const Red = Wec / Math.sin(aStair2);
 		// step-5 : checks on the parameter values
-		if (R1 < Ri) {
-			throw `err071: D1 ${param.D1} is too small compare to Di ${param.Di}`;
+		if (param.Wi2 < param.Wi1) {
+			throw `err092: Wi2 ${param.Wi2} is too small compare to Wi1 ${param.Wi1}`;
+		}
+		if (param.We2 < param.We1) {
+			throw `err095: We2 ${param.We2} is too small compare to We1 ${param.We1}`;
+		}
+		if (param.W < R1) {
+			throw `err098: D1 ${param.D1} is too small compare to Wi2 ${param.Wi2}`;
 		}
 		// step-6 : any logs
-		rGeome.logstr += `Surface ${ffix(surface)} mm2, volume ${ffix(volume)} mm3\n`;
+		rGeome.logstr += `Stair angle ${ffix(radToDeg(2 * aStair2))} degree\n`;
+		rGeome.logstr += `Stairs angle ${ffix(param.Nn / param.Nd)} turn\n`;
 		// sub-function
-		// figCylinder
-		const ctrCylinder = contourCircle(0, 0, R1);
-		const ctrHole = contourCircle(0, 0, Ri);
-		figCylinder.addMainOI([ctrCylinder, ctrHole]);
-		// figHeight
-		figHeight.addMainO(ctrRectangle(-R1, 0, 2 * R1, param.T1));
-		figHeight.addSecond(ctrRectangle(-Ri, 0, 2 * Ri, param.T1));
+		// figTop
+		const ctrCircleRef = contourCircle(0, 0, R1);
+		const ctrCircleSpiralI = contourCircle(0, 0, Rid);
+		const ctrCircleSpiralE = contourCircle(0, 0, Red);
+		figTop.addSecond(ctrCircleRef);
+		figTop.addSecond(ctrCircleSpiralI);
+		figTop.addSecond(ctrCircleSpiralE);
+		// figBorderI
+		// figBorderE
 		// final figure list
 		rGeome.fig = {
-			faceCylinder: figCylinder,
-			faceHeight: figHeight
+			faceTop: figTop,
+			faceBorderI: figBorderI,
+			faceBorderE: figBorderE
 		};
 		// volume
 		const designName = rGeome.partName;
 		rGeome.vol = {
 			extrudes: [
 				{
-					outName: `subpax_${designName}_cyl`,
+					outName: `subpax_${designName}_top`,
 					face: `${designName}_faceCylinder`,
 					extrudeMethod: EExtrude.eLinearOrtho,
-					length: param.T1,
+					length: param.H1,
 					rotate: [0, 0, 0],
 					translate: [0, 0, 0]
 				}
@@ -114,7 +135,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 				{
 					outName: `pax_${designName}`,
 					boolMethod: EBVolume.eUnion,
-					inList: [`subpax_${designName}_cyl`]
+					inList: [`subpax_${designName}_top`]
 				}
 			]
 		};
