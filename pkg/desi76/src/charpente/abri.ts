c1a6b4c4806363c1bf748a808d3acd64f4a05575
charlyoleg <charles.braquet@gmx.fr>
2025-10-06 17:48:04 +0200

    W1a and W1b

--- pkg/desi76/src/charpente/abri.ts
+++ pkg/desi76/src/charpente/abri.ts
@@ -3,7 +3,7 @@
 
 import type {
 	//tContour,
-	tOuterInner,
+	//tOuterInner,
 	tParamDef,
 	tParamVal,
 	tGeom,
@@ -14,12 +14,13 @@ import type {
 import {
 	contour,
 	contourCircle,
+	ctrRectangle,
 	figure,
 	//degToRad,
 	//radToDeg,
-	//ffix,
+	ffix,
 	pNumber,
-	//pCheckbox,
+	pCheckbox,
 	//pDropdown,
 	pSectionSeparator,
 	EExtrude,
@@ -31,17 +32,50 @@ const pDef: tParamDef = {
 	partName: 'abri',
 	params: [
 		//pNumber(name, unit, init, min, max, step)
-		pNumber('H1', 'mm', 40, 1, 4000, 1),
-		pNumber('H2', 'mm', 50, 1, 4000, 1),
-		pNumber('radius', 'mm', 10, 1, 4000, 1),
-		pSectionSeparator('corners'),
-		pNumber('Rc', 'mm', 10, 0, 400, 1)
+		pNumber('Na', 'pole', 2, 2, 20, 1),
+		pNumber('Nb', 'pole', 3, 2, 20, 1),
+		pNumber('Lb', 'mm', 3000, 500, 10000, 1),
+		pNumber('La', 'mm', 3000, 500, 10000, 1),
+		pNumber('Ka', 'mm', 1000, 0, 10000, 1),
+		pNumber('Ja', 'mm', 1000, 0, 10000, 1),
+		pSectionSeparator('Side optiions'),
+		pCheckbox('SecondPoleNorth', false),
+		pCheckbox('SecondPoleSouth', false),
+		pSectionSeparator('pole heights'),
+		pNumber('H1', 'mm', 2500, 1500, 5000, 1),
+		pNumber('H2', 'mm', 300, 10, 1000, 1),
+		pNumber('H3', 'mm', 300, 10, 1000, 1),
+		pSectionSeparator('pole widths'),
+		pNumber('W1a', 'mm', 300, 10, 1000, 1),
+		pNumber('W1b', 'mm', 300, 10, 1000, 1),
+		pNumber('V1', 'mm', 40, 0, 1000, 1),
+		pNumber('U1', 'mm', 40, 0, 1000, 1),
+		pNumber('W2', 'mm', 150, 5, 1000, 1),
+		pNumber('W3', 'mm', 150, 5, 1000, 1),
+		pSectionSeparator('pole holes'),
+		pNumber('D2', 'mm', 20, 0, 200, 1),
+		pNumber('D3', 'mm', 20, 0, 200, 1)
 	],
 	paramSvg: {
-		H1: 'abri_base.svg',
-		H2: 'abri_base.svg',
-		radius: 'abri_base.svg',
-		Rc: 'abri_beam_heights.svg'
+		Na: 'abri_base.svg',
+		Nb: 'abri_base.svg',
+		Lb: 'abri_base.svg',
+		La: 'abri_base.svg',
+		Ka: 'abri_base.svg',
+		Ja: 'abri_base.svg',
+		SecondPoleNorth: 'abri_base.svg',
+		SecondPoleSouth: 'abri_base.svg',
+		H1: 'abri_beam_heights.svg',
+		H2: 'abri_beam_heights.svg',
+		H3: 'abri_beam_heights.svg',
+		W1a: 'abri_base.svg',
+		W1b: 'abri_base.svg',
+		V1: 'abri_base.svg',
+		U1: 'abri_base.svg',
+		W2: 'abri_base.svg',
+		W3: 'abri_base.svg',
+		D2: 'abri_base.svg',
+		D3: 'abri_base.svg'
 	},
 	sim: {
 		tMax: 180,
@@ -52,24 +86,69 @@ const pDef: tParamDef = {
 
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
-	const figFace = figure();
+	const figBase = figure();
+	const figPoleFace = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
-		// figFace
-		const face1: tOuterInner = [];
-		const ctrPoleFace = contour(-param.H1 / 2, -param.H2 / 2)
-			.addCornerRounded(param.Rc)
-			.addSegStrokeA(param.H1 / 2, -param.H2 / 2)
-			.addSegStrokeA(param.H1 / 2, param.H2 / 2)
-			.addCornerRounded(param.Rc)
-			.addSegStrokeA(-param.H1 / 2, param.H2 / 2)
+		// step-4 : some preparation calculation
+		const H2H3 = param.H2 + param.H3;
+		const W1V1 = param.W1a - 2 * param.V1;
+		const W1U1 = param.W1b - 2 * param.U1;
+		const poleSecondNb = (param.SecondPoleNorth ? 1 : 0) + (param.SecondPoleSouth ? 1 : 0);
+		const poleNbWEmin = 2 + poleSecondNb;
+		const lb = (param.Nb * param.W1b + (param.Nb - 1) * param.Lb) / 1000; // m
+		const laPre = param.Na * param.W1a + (param.Na - 1 - poleSecondNb) * param.La; // m
+		const la = (laPre + poleSecondNb * param.Ka) / 1000; // m
+		const lbInner = lb - 2 * param.W1b / 1000;
+		const laInner = la - 2 * param.W1a / 1000;
+		const lbRoof = lb;
+		const laRoof = la + 2 * param.Ja / 1000;
+		const stepX = param.W1b + param.lb;
+		// step-5 : checks on the parameter values
+		if (W1V1 < 1) {
+			throw `err096: W1a ${param.W1a} is too small compare to V1 ${param.V1} mm`;
+		}
+		if (W1U1 < 1) {
+			throw `err099: W1b ${param.W1b} is too small compare to U1 ${param.U1} mm`;
+		}
+		if (param.Na < poleNbWEmin) {
+			throw `err103: Na ${param.Na} must be bigger than ${poleNbWEmin} poles`;
+		}
+		// step-6 : any logs
+		rGeome.logstr += `Inner size X: ${ffix(lbInner)} Y: ${ffix(laInner)} m, S: ${ffix(lbInner * laInner)} m2\n`;
+		rGeome.logstr += `Wall size X: ${ffix(lb)} Y: ${ffix(la)} m, S: ${ffix(lb * la)} m2\n`;
+		rGeome.logstr += `Roof size X: ${ffix(lbRoof)} Y: ${ffix(laRoof)} m, S: ${ffix(lbRoof * laRoof)} m2\n`;
+		// step-7 : drawing of the figures
+		// sub-functions
+		// figBase
+		let yj = 0;
+		for (let jj = 0; jj < param.Na; jj++) {
+			for (let ii = 0; ii < param.Nb; ii++) {
+				figBase.addMainO(ctrRectangle(ii * stepX, yj, param.W1b, param.W1a));
+			}
+			if (jj === 0) {
+				yj += param.W1a + param.SecondPoleSouth ? param.Ka : param.La;
+			} else if (jj === param.Na - 2) {
+				yj += param.W1a + param.SecondPoleNorth ? param.Ka : param.La;
+			} else {
+				yj += param.W1a + param.La;
+			}
+		}
+		// figPoleFace
+		const ctrPoleFace = contour(0, 0)
+			.addSegStrokeR(param.W1a, 0)
+			.addSegStrokeR(0, param.H1)
+			.addSegStrokeR(-param.V1, 0)
+			.addSegStrokeR(0, H2H3)
+			.addSegStrokeR(W1V1, 0)
+			.addSegStrokeR(0, -H2H3)
+			.addSegStrokeR(-param.V1, 0)
 			.closeSegStroke();
-		face1.push(ctrPoleFace);
-		face1.push(contourCircle(0, 0, param.radius));
-		figFace.addMainOI(face1);
+		figPoleFace.addMainOI([ctrPoleFace, contourCircle(0, 0, param.radius)]);
 		// final figure list
 		rGeome.fig = {
-			faceVoila: figFace
+			faceBase: figBase,
+			facePoleFace: figPoleFace
 		};
 		// volume
 		const designName = rGeome.partName;
@@ -77,7 +156,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			extrudes: [
 				{
 					outName: `subpax_${designName}_top`,
-					face: `${designName}_faceVoila`,
+					face: `${designName}_facePoleFace`,
 					extrudeMethod: EExtrude.eLinearOrtho,
 					length: 10,
 					rotate: [0, 0, 0],
