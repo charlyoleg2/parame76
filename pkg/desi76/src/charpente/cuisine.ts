fea13857828c6a8b6cc9daadb6de862b258db0df
charlyoleg <charles.braquet@gmx.fr>
2026-04-06 19:34:25 +0200

    cuisine top

--- pkg/desi76/src/charpente/cuisine.ts
+++ pkg/desi76/src/charpente/cuisine.ts
@@ -25,7 +25,7 @@ import {
 	//vector,
 	contour,
 	//contourCircle,
-	//ctrRectangle,
+	ctrRectangle,
 	figure,
 	degToRad,
 	radToDeg,
@@ -49,16 +49,25 @@ const pDef: tParamDef = {
 		//pNumber(name, unit, init, min, max, step)
 		pNumber('AX', 'cm', 274, 200, 600, 1),
 		pNumber('AY', 'cm', 210, 100, 500, 1),
-		pNumber('BY', 'cm', 100, 50, 500, 1),
-		pNumber('CX', 'cm', 400, 50, 800, 1),
+		pNumber('BY', 'cm', 130, 50, 500, 1),
+		pNumber('CX', 'cm', 405, 50, 800, 1),
+		pNumber('DPX', 'cm', 108, 50, 500, 1),
 		pSectionSeparator('Window'),
 		pNumber('WW', 'cm', 94, 40, 200, 1),
+		pNumber('WH', 'cm', 90, 40, 200, 1),
+		pNumber('WPH', 'cm', 120, 40, 200, 1),
+		pNumber('WPX', 'cm', 130, 40, 400, 1),
+		pNumber('WPY', 'cm', 270, 40, 400, 1),
+		pSectionSeparator('Door'),
+		pNumber('DW', 'cm', 76, 40, 200, 1),
+		pNumber('DH', 'cm', 210, 180, 300, 1),
+		pNumber('AMH', 'cm', 300, 100, 600, 1),
 		pSectionSeparator('Roof'),
 		pNumber('MW', 'cm', 10, 5, 50, 1),
-		pNumber('eMH', 'cm', 85, 5, 50, 1),
+		pNumber('eMH', 'cm', 90, 5, 200, 1),
 		pNumber('eRA', 'degree', 45, 10, 85, 1),
 		pNumber('eRW', 'cm', 250, 100, 500, 1),
-		pNumber('nMH', 'cm', 85, 5, 50, 1),
+		pNumber('nMH', 'cm', 90, 5, 200, 1),
 		pNumber('nRA', 'degree', 45, 10, 85, 1),
 		pNumber('nRW', 'cm', 200, 100, 500, 1)
 	],
@@ -92,16 +101,20 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		// step-4 : some preparation calculation
 		const aRe = degToRad(param.eRA);
 		const aRn = degToRad(param.nRA);
-		const lX = param.AX + param.CX;
-		const lY = param.AY + param.BY;
+		const lX = param.AX + param.CX + param.MW;
+		const lY = param.AY + param.BY + param.MW;
+		const lAZx = param.AX + param.MW - param.DPX - param.DW;
+		const lR2e = (200 - param.eMH) / Math.tan(aRe);
+		const lR2n = (200 - param.nMH) / Math.tan(aRn);
+		const cWHtop = 30;
 		// step-5 : checks on the parameter values
-		if (lX < 0) {
-			throw `err099: sign of lX ${ffix(lX)} must be positive`;
+		if (lAZx < 0) {
+			throw `err099: sign of lAZx ${ffix(lAZx)} must be positive`;
 		}
 		// step-6 : any logs
 		rGeome.logstr += `lX ${ffix(lX)}  lY ${ffix(lY)}\n`;
-		rGeome.logstr += `East side: roof-angle ${ffix(radToDeg(aRe))}\n`;
-		rGeome.logstr += `North side: roof-angle ${ffix(radToDeg(aRn))}\n`;
+		rGeome.logstr += `East side: roof-angle ${ffix(radToDeg(aRe))}, lR2e ${ffix(lR2e)}\n`;
+		rGeome.logstr += `North side: roof-angle ${ffix(radToDeg(aRn))}, lR2n ${ffix(lR2n)}\n`;
 		// step-7 : drawing of the figures
 		// fig1
 		const ctrBase = contour(0, 0)
@@ -109,7 +122,40 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			.addSegStrokeR(0, lY)
 			.addSegStrokeR(-lX, 0)
 			.closeSegStroke();
+		const ctrArbeitZimmerS = contour(0, param.AY - param.MW)
+			.addSegStrokeR(lAZx, 0)
+			.addSegStrokeR(0, param.MW)
+			.addSegStrokeR(-lAZx, 0)
+			.closeSegStroke();
+		const ctrArbeitZimmerN = contour(param.AX, 0)
+			.addSegStrokeR(param.MW, 0)
+			.addSegStrokeR(0, param.AY)
+			.addSegStrokeR(-param.DPX, 0)
+			.addSegStrokeR(0, -param.MW)
+			.addSegStrokeR(param.DPX - param.MW, 0)
+			.closeSegStroke();
+		const ctrArbeitZimmerTop = contour(param.AX, 0)
+			.addSegStrokeR(param.MW, 0)
+			.addSegStrokeR(0, param.AY)
+			.addSegStrokeR(-param.AX - param.MW, 0)
+			.addSegStrokeR(0, -param.MW)
+			.addSegStrokeR(param.AX, 0)
+			.closeSegStroke();
+		const ctr2M = contour(0, 0)
+			.addSegStrokeR(lX, 0)
+			.addSegStrokeR(0, lY)
+			.addSegStrokeR(-lR2n, 0)
+			.addSegStrokeR(0, -lY + lR2e)
+			.addSegStrokeR(-lX + lR2n, 0)
+			.closeSegStroke();
 		figTop.addMainO(ctrBase);
+		figTop.addSecond(ctrArbeitZimmerS);
+		figTop.addSecond(ctrArbeitZimmerN);
+		figTop.addSecond(ctrArbeitZimmerTop);
+		figTop.addSecond(ctr2M);
+		figTop.addSecond(ctrRectangle((param.AX - param.WW) / 2, 0, param.WW, cWHtop));
+		figTop.addSecond(ctrRectangle(lX - param.WW - param.WPX, 0, param.WW, cWHtop));
+		figTop.addSecond(ctrRectangle(lX - cWHtop, param.WPY, cWHtop, param.WW));
 		// final figure list
 		rGeome.fig = {
 			faceTop: figTop
