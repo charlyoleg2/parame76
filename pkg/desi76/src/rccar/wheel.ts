55dba3af51dce86927d124d90aa20fdd34d615ff
charlyoleg <charles.braquet@gmx.fr>
2026-07-11 19:03:41 +0200

    params of wheel

--- pkg/desi76/src/rccar/wheel.ts
+++ pkg/desi76/src/rccar/wheel.ts
@@ -45,15 +45,35 @@ const pDef: tParamDef = {
 	partName: 'wheel',
 	params: [
 		//pNumber(name, unit, init, min, max, step)
-		pNumber('D1', 'mm', 60, 1, 1000, 1),
+		pNumber('D1', 'mm', 20, 1, 1000, 0.1),
+		pNumber('RD2', 'mm', 1, 1, 500, 1),
+		pNumber('RD3', 'mm', 5, 1, 500, 1),
+		pNumber('RD4', 'mm', 2, 1, 500, 1),
+		pNumber('RD5', 'mm', 20, 1, 500, 1),
+		pNumber('RD6', 'mm', 4, 1, 500, 1),
+		pNumber('N6', 'teeth', 20, 5, 1000, 1),
 		pSectionSeparator('widths'),
-		pNumber('W1', 'mm', 2, 0, 500, 1),
-		pNumber('W2', 'mm', 2, 0, 500, 1)
+		pNumber('W1', 'mm', 1, 0, 500, 1),
+		pNumber('W2', 'mm', 20, 1, 500, 1),
+		pNumber('W3', 'mm', 0, 0, 500, 1),
+		pNumber('W4', 'mm', 2, 1, 500, 1),
+		pNumber('W5', 'mm', 2, 1, 500, 1),
+		pNumber('W6', 'mm', 1, 0, 500, 1)
 	],
 	paramSvg: {
 		D1: 'wheel_side.svg',
+		RD2: 'wheel_side.svg',
+		RD3: 'wheel_cut.svg',
+		RD4: 'wheel_cut.svg',
+		RD5: 'wheel_side.svg',
+		RD6: 'wheel_side.svg',
+		N6: 'wheel_side.svg',
 		W1: 'wheel_cut.svg',
-		W2: 'wheel_cut.svg'
+		W2: 'wheel_cut.svg',
+		W3: 'wheel_cut.svg',
+		W4: 'wheel_cut.svg',
+		W5: 'wheel_cut.svg',
+		W6: 'wheel_cut.svg'
 	},
 	sim: {
 		tMax: 180,
@@ -65,28 +85,40 @@ const pDef: tParamDef = {
 // step-3 : definition of the function that creates from the parameter-values the figures and construct the 3D
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
-	const figPneuSide = figure();
+	const figTube = figure();
+	const figPneu = figure();
+	const figTrans1 = figure();
+	const figTrans2 = figure();
+	const figCut = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
 		const R1 = param.D1 / 2;
 		//const pi2 = Math.PI / 2;
 		//const epsilon = 0.01;
-		const Dpneu = param.D1 + 2 * (param.RD2 + param.RD5 + param.RD6);
-		const Dtrans = param.D1 + 2 * (param.RD2 + param.RD3 + param.RD4);
+		const Rpneu = R1 + param.RD2 + param.RD5 + param.RD6;
+		const Rtrans = R1 + param.RD2 + param.RD3 + param.RD4;
 		const Wtot = param.W1 + param.W2 + param.W3 + 2 * param.W4 + param.W5 + param.W6;
 		// step-5 : checks on the parameter values
-		if (Dpneu < Dtrans) {
-			throw `err230: Dpneu ${ffix(Dpneu)} is too small compare to Dtrans ${ffix(Dtrans)}`;
+		if (Rpneu < Rtrans) {
+			throw `err230: Dpneu ${ffix(2 * Rpneu)} is too small compare to Dtrans ${ffix(2 * Rtrans)}`;
 		}
 		// step-6 : any logs
-		rGeome.logstr += `Wtot ${ffix(Wtot)}  Dpneu ${ffix(Dpneu)}  Dtrans ${ffix(Dtrans)} mm\n`;
+		rGeome.logstr += `Wtot ${ffix(Wtot)}  Dpneu ${ffix(2 * Rpneu)}  Dtrans ${ffix(2 * Rtrans)} mm\n`;
 		// step-7 : drawing of the figures
-		// figSide
-		figPneuSide.addMainO(contourCircle(0, 0, R1));
+		// figTube
+		figTube.addMainO(contourCircle(0, 0, R1));
+		// figPneu
+		// figTrans1
+		// figTrans2
+		// figCut
 		// final figure list
 		rGeome.fig = {
-			facePneuSide: figPneuSide
+			faceTube: figTube,
+			facePneu: figPneu,
+			faceTrans1: figTrans1,
+			faceTrans2: figTrans2,
+			faceCut: figCut
 		};
 		// step-8 : recipes of the 3D construction
 		// volume
@@ -98,7 +130,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			const eName = `subpax_${designName}_pneu`;
 			partExtrude.push({
 				outName: eName,
-				face: `${designName}_facePneuSide`,
+				face: `${designName}_facePneu`,
 				extrudeMethod: EExtrude.eLinearOrtho,
 				length: param.W2,
 				rotate: [0, 0, 0],
